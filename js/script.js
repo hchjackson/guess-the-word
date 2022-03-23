1604c53cc4e2435bcc35de2c7d9a00bba658c7e9
Hui Chieh_J <HuiChieh.Hsiang@gmail.com>
2022-03-23 14:00:06 -0400

    Updated variable names, added functions to validate input and capture the guessed letter

--- js/script.js
+++ js/script.js
@@ -1,30 +1,75 @@
-const guessedLetters = document.querySelector(".guessed-letters"); // The unordered list where the player’s guessed letters will appear.
-const guessButton = document.querySelector(".guess"); // The button with the text “Guess!” in it.
+const guessedLettersElement = document.querySelector(".guessed-letters"); // The unordered list where the player’s guessed letters will appear.
+const guessLetterButton = document.querySelector(".guess"); // The button with the text “Guess!” in it.
 const letterInput = document.querySelector(".letter"); // The text input where the player will guess a letter.
 const wordInProgress = document.querySelector(".word-in-progress"); // The empty paragraph where the word in progress will appear.
-const remainingGuesses = document.querySelector(".remaining"); // The paragraph where the remaining guesses will display.
-const remainingSpan = document.querySelector(".remaining span"); // The span inside the paragraph where the remaining guesses will display.
+const remainingGuessesElement = document.querySelector(".remaining"); // The paragraph where the remaining guesses will display.
+const remainingGuessesSpan = document.querySelector(".remaining span"); // The span inside the paragraph where the remaining guesses will display.
 const message = document.querySelector(".message"); // The empty paragraph where messages will appear when the player guesses a letter.
 const playAgainButton = document.querySelector(".play-again"); // The hidden button that will appear prompting the player to play again.
 
 const word = "magnolia"; // For testing out the game
+const guessedLetters = []; // Will contain all the letters the player guesses
 
-// To add and display placeholders for chosen word's letters
-const placeholder = function (mysteryWord) {
-  const mysteryArray = [];
-  for (const letter of mysteryWord) {
+// Display our symbols as placeholders for chosen word's letters
+const placeholder = function (word) {
+  const placeholderLetters = [];
+  for (const letter of word) {
     console.log(letter);
-    mysteryArray.push("●");
+    placeholderLetters.push("●");
   }
-  wordInProgress.innerText = mysteryArray.join("");
+  wordInProgress.innerText = placeholderLetters.join("");
 };
 
 placeholder(word);
 
 // Add event listener for when play clicks Guess button
-guessButton.addEventListener("click", function (e) {
+guessLetterButton.addEventListener("click", function (e) {
   e.preventDefault();
-  const mysteryLetter = letterInput.value;
-  console.log(mysteryLetter);
+  // Empty message paragraph
+  message.innerText = "";
+  // Let's grab what was entered in the letterInput
+  const guess = letterInput.value;
+  // Let's make sure that it's a single letters
+  const goodGuess = validateInput(guess);
+
+  if (goodGuess) {
+    // We've got a letter! Let's guess!
+    makeGuess(guess);
+  }
   letterInput.value = "";
 });
+
+// Check player's input to only allow letters, no numbers or symbols
+const validateInput = function (input) {
+  const acceptedLetter = /[a-zA-Z]/; // Regular expression to ensure input is a letters
+
+  if (input.length === 0) {
+    // Scenario 1 - Is the input empty?
+    message.innerText = "Please enter a letter.";
+  }
+  else if (input.length > 1) {
+    // Scenario 2 - Did you type more than one letter?
+    message.innerText = "Please enter a single letter.";
+  }
+  else if(!input.match(acceptedLetter)) { // Does the input not match regular expression pattern
+    // Scenario 3 - Did you type a number, a special character or some other non letter thing?
+    message.innerText = "Please enter a letter from A to Z.";
+  }
+  else {
+    // Scenerio 4 - We finally got a single letter, omg yay
+    return input;
+  }
+};
+
+// Capture input
+const makeGuess = function (guess) {
+  guess = guess.toUpperCase(); // convert guess to uppercase
+  // check to see if array contains letter
+  if (guessedLetters.includes(guess)) {
+    message.innerText = "You already guessed that letter, silly. Try again.";
+  }
+  else {
+    guessedLetters.push(guess);
+    console.log(guessedLetters);
+  }
+};
